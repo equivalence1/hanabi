aa41626fc14c29cfeb9004eeb856adb911fef213
Frommi <daniil.liferenko@gmail.com>
2014-07-29 21:39:40 +0400

    some gameState stuff

--- hanabi/main.py
+++ hanabi/main.py
@@ -27,6 +27,8 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 
+card_amount_in_deck_by_value = [0, 3, 2, 2, 2, 1]
+card_amount_in_hand_by_user_count = [0, 0, 5, 5, 4, 4]
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -56,14 +58,36 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 
 
+class Card(ndb.Model):
+    color = ndb.IntegerProperty(indexed=False)
+    value = ndb.IntegerProperty(indexed=False)
+
+
+class Hand(ndb.Model):
+    cards = ndb.LocalStructuredProperty(Card, indexed=False, repeated=True)
+
+
+class GameState(ndb.Model):
+    deck = ndb.StructuredProperty(Card, indexed=False, repeated=True)
+    solitaire = ndb.StructuredProperty(Card, indexed=False, repeated=True)
+    junk = ndb.StructuredProperty(Card, indexed=False, repeated=True)
+    user_hands = ndb.LocalStructuredProperty(Hand, indexed=False, repeated=True)
+
+    life_count = ndb.IntegerProperty()
+    hint_count = ndb.IntegerProperty()
+
+    whose_move = ndb.IntegerProperty()
+
+
 class Game(ndb.Model):
     name = ndb.StringProperty(indexed=False)
     password = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
     user_id_list = ndb.StringProperty(indexed=False, repeated=True)
-    seed = ndb.IntegerProperty(indexed=False)
 
     max_user_count = ndb.IntegerProperty(indexed=False)
+
+    game_state = ndb.StructuredProperty(GameState, indexed=False)
 
 
 class GameCreateHandler(webapp2.RequestHandler):
@@ -77,6 +101,8 @@ class GameCreateHandler(webapp2.RequestHandler):
 
         games = Game.query(ancestor=game_key(game_name)).fetch()
         if (len(games) != 0):
+            logging.info("Game with name " + game_name + " already exists")
+            channel.send_message(user_id, "error?msg=Game with name " + game_name + " already exists")
             return
 
         game = Game(parent=game_key(game_name))
@@ -84,7 +110,6 @@ class GameCreateHandler(webapp2.RequestHandler):
         game.name = game_name
         game.password = password
         game.user_id_list.append(user_id)
-        game.seed = random.randint(0, (1 << 31) - 1)
         game.max_user_count = int(max_user_count)
         game.put()
 
@@ -98,7 +123,6 @@ class JoinGame(webapp2.RequestHandler):
         user_id = self.request.get("user_id")        
         game_name = self.request.get("game_name")
         entered_password = self.request.get("game_password")
-
 
         games = Game.query(ancestor=game_key(game_name)).fetch(1)
         if (len(games) != 1):
@@ -138,9 +162,44 @@ class SendChatMessage(webapp2.RequestHandler):
             channel.send_message(user_id, "new_message?message=" + message)
 
 
+class GameHandler(webapp2.RequestHandler):
+    def post(self):
+        logging.info("GameHandler post")
+
+        game_name = self.request.get("game_name")
+
+        game = Game.query(ancestor=game_key(game_name)).fetch(1)[0]
+
+        game.game_state.deck = []
+        for color in range(1, 6):
+            for value in range(1, 6):
+                for count in range(0, card_amount_in_deck_by_value[value]):
+                    game.game_state.deck.append(Card(color=color, value=value))
+
+        random.shuffle(game.game_state.deck)
+
+        game.game_state.solitaire = []
+        game.game_state.junk = []
+        game.game_state.life_count = 3
+        game.game_state.hint_count = 8
+
+        game.game_state.user_hands = []
+        for hand_num in range(len(game.user_id_list)):
+            hand = []
+            for card_num in range(card_amount_in_hand_by_user_count[len(game.user_id_list)]):
+                hand.append(game.game_state.deck.pop())
+            game.game_state.user_hands = hand
+
+        game.game_state.whose_move = random.randint(0, len(game.user_id_list) - 1)
+
+        for user_id in game.user_id_list:
+            channel.send_message(user_id, "start_game&")
+
+
 application = webapp2.WSGIApplication([
     ("/", MainPage),
     ("/game_create", GameCreateHandler),
     ("/join_game", JoinGame),
+    ("/game", GameHandler),
     ("/send_chat_message", SendChatMessage)
 ], debug=True)
